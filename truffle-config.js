89be517b9408f8b16386ba666df5f5280671f045
speckofstardust <sharmada03@gmail.com>
2024-11-09 13:09:28 +0530

    config changes

--- truffle-config.js
+++ truffle-config.js
@@ -6,10 +6,10 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
-      network_id: "*",
+      network_id: "5777",
       host: "127.0.0.1",
-      // port: 7545, // for ganache gui
-      port: 8545, // for ganache-cli
+      // port: 8545, // for ganache cli
+      port: 7545, // for ganache-gui
       gas: 6721975,
       gasPrice: 20000000000,
     },
